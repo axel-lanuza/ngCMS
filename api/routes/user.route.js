f1f8d471027abed826cdb56161bedcd11ee8f321
RN Kushwaha <rn.kushwaha022@gmail.com>
2019-08-18 01:32:55 +0530

    removed unused code

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 var bcrypt = require('bcryptjs');
 var mysql = require('../config/mysql');
 
